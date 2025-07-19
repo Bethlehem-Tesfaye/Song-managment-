d3847a029cdc9dd9bba50dd3a6384c77b55b8fee
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-07-20 02:19:23 +0400

    style: fix formatting to pass Prettier CI

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,11 @@
-import  mongoose  from "mongoose";
+import mongoose from "mongoose";
 
-const DbConnect = async ()=>{
-    mongoose.connection.on('connected', ()=>{
-        console.log('database connected');
-        
-    })
+const DbConnect = async () => {
+  mongoose.connection.on("connected", () => {
+    console.log("database connected");
+  });
 
-    await mongoose.connect(`${process.env.MONGODB_URL}`)
-}
+  await mongoose.connect(`${process.env.MONGODB_URL}`);
+};
 
 export default DbConnect;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
-import express from "express"
+import express from "express";
 import DbConnect from "./db/db.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
 
 DbConnect();
-app.listen(port,()=>{
-    console.log(`server runiing on port: ${port}`);
-    
-})
+app.listen(port, () => {
+  console.log(`server runiing on port: ${port}`);
+});
