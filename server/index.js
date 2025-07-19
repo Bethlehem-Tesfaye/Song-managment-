d9610ea43dbd3d2526a658fc5fa6d61b0242766c
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2025-07-20 03:48:24 +0400

    feat: implement controllers and routers with working API endpoints

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 import express from "express";
 import DbConnect from "./db/db.js";
 import dotenv from "dotenv";
+import router from "./routes/songRoutes.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
+import cors from "cors";
+
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || true,
+    credentials: true
+  })
+);
+
+app.use(express.json());
+app.use('/api/songs', router)
+app.use(errorMiddleware)
 
 DbConnect();
 app.listen(port, () => {
---
+++ server/routes/songRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { createSong, deleteSong, getSongs, updateSong } from '../controllers/songController.js';
+
+const router = express.Router();
+
+router.post('/', createSong)
+router.get('/', getSongs)
+router.put('/:id', updateSong)
+router.delete('/:id', deleteSong)
+
+export default router
